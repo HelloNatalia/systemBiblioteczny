3a0c6f2f4cb4025e12c9d2b4e85c07395ab051d2
HelloNatalia <nataliatucka01@gmail.com>
2023-02-15 13:22:11 +0100

    searching book with autocomplete at reader's page

--- yii-application/backend/views/readers/_search_r.php
+++ yii-application/backend/views/readers/_search_r.php
@@ -2,10 +2,20 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kartik\select2\Select2;
+use backend\models\Borrow;
+use yii\helpers\ArrayHelper;
 
 $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']])?>
 
-<?= $form->field($searchModel, 'title')->textInput()?>
+<?= $form->field($searchModel, 'id')->widget(Select2::classname(), [
+    'data' => ArrayHelper::map(Borrow::find()->leftJoin('books', 'books.id = borrow.book_id')->andWhere(['borrow.returned' => 0])->andWhere(['borrow.reader_id' => $id])->orderBy(['books.title' => SORT_ASC])->all(), 'book.id', 'book.title'),
+    'options' => ['placeholder' => 'Wyszukaj tytułu'],
+    'pluginOptions' => [
+        'allowClear' => true,
+        'width' => '500px'
+    ],
+])->label("Tytuł") ?>
 
 <?= Html::submitButton('Szukaj')?>
 <?= Html::a('Pokaż wszystko', ['reader', 'clear' => 1, 'id' => $id], ['class' => 'btn btn-primary btn-sm']) ?>
