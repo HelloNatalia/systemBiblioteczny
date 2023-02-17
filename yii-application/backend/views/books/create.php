34b735222fc226b3e26b52b84d6e3fd61b0145cd
HelloNatalia <nataliatucka01@gmail.com>
2023-02-17 14:07:31 +0100

    form for create book

--- yii-application/backend/views/books/create.php
+++ yii-application/backend/views/books/create.php
@@ -9,42 +9,73 @@ use yii\helpers\ArrayHelper;
 
 $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']])?>
 
-<?= $form->field($books, 'title')->textInput()?>
+<p class="display-5 fs-3 mt-3">Dodaj nową książkę</p>
 
-<?= $form->field($autors, 'name')->textInput()?>
-<?= $form->field($autors, 'surname')->textInput()?>
+<div class="form-group">
+    <div class="row p-4 mt-2">
+        <div class="col-12">
+            <?= $form->field($books, 'title')->textInput()->label('Tytuł książki')?>
+        </div>
+        <div class="col-12 col-md-6 mt-3">
+            <?= $form->field($autors, 'name')->textInput()->label('Imię autora')?>
+        </div>
+        <div class="col-12 col-md-6 mt-3">
+            <?= $form->field($autors, 'surname')->textInput()->label('Nazwisko autora')?>
+        </div>
+        <hr class="mt-5 mb-4">
+        <div class="col-12 col-md-5 col-lg-4 mt-3">
+            <?= $form->field($autors, 'country')->widget(Select2::classname(), [
+                'data' => ArrayHelper::map(Countries::find()->select('name')->orderBy(['name' => SORT_ASC])->all(), 'name', 'name'),
+                'options' => ['placeholder' => 'Wybierz kraj'],
+                'pluginOptions' => [
+                    'allowClear' => true,
+                ],
+            ])->label("Kraj pochodzenia autora") ?>
+        </div>
+        <div class="col-12 col-md-5 col-lg-4 mt-3">
+            <?= $form->field($books, 'category_id')->widget(Select2::classname(), [
+                'data' => ArrayHelper::map(Categories::find()->orderBy(['category_name' => SORT_ASC])->all(), 'id', 'category_name'),
+                'options' => ['placeholder' => 'Wybierz kategorię'],
+                'pluginOptions' => [
+                    'allowClear' => true,
+                ],
+            ])->label("Kategoria") ?>
+        </div>
+        <div class="col-12 col-md-2 col-lg-4 mt-3">
+            <?= $form->field($books, 'publ_year')->textInput(['type' => 'number', ['maxlength' => 4]])->label('Rok')?>
+        </div>
+        <div class="col-12 mt-3">
+            <?= $form->field($books, 'description')->textArea()->label('Opis')?>
+        </div>
+        <hr class="mt-5 mb-4">
+        <div class="col-2 mt-3">
+            <?= $form->field($books, 'quantity')->textInput(['type' => 'number'])->label('Ilość')?>
+        </div>
+        <div class="col-2 mt-3">
+            <?= $form->field($books, 'img')->fileinput(['multiple' => false, 'accept' => 'image/*'])->label('Okładka')?>
+        </div>
+        <div class="col-8"></div>
+        <div class="col-12 mt-4">
+            <?= Html::submitButton('Utwórz nową książkę', ['class' => 'btn btn-success btn-md'])?>
+        </div>
+    </div>
+</div>
 
-<?= $form->field($autors, 'country')->widget(Select2::classname(), [
-    'data' => ArrayHelper::map(Countries::find()->select('name')->orderBy(['name' => SORT_ASC])->all(), 'name', 'name'),
-    'options' => ['placeholder' => 'Wybierz kraj'],
-    'pluginOptions' => [
-        'allowClear' => true,
-        'width' => '500px'
-    ],
-])->label("Kraj pochodzenia autora") ?>
 
-<?= $form->field($books, 'category_id')->widget(Select2::classname(), [
-    'data' => ArrayHelper::map(Categories::find()->orderBy(['category_name' => SORT_ASC])->all(), 'id', 'category_name'),
-    'options' => ['placeholder' => 'Wybierz kategorię'],
-    'pluginOptions' => [
-        'allowClear' => true,
-        'width' => '500px'
-    ],
-])->label("Kategoria") ?>
-
-
-
-<?= $form->field($books, 'publ_year')->textInput(['type' => 'number', ['maxlength' => 4]])->label('Publication year')?>
-
-<?= $form->field($books, 'description')->textArea()?>
-<?= $form->field($books, 'img')->fileinput(['multiple' => false, 'accept' => 'image/*'])?>
-<?= $form->field($books, 'quantity')->textInput(['type' => 'number'])?>
-
-<?= Html::submitButton('Post')?>
 
 <?php ActiveForm::end()?>
 
-
+<style>
+    div .help-block {
+        color: red;
+    }
+    hr {
+        height:4px; 
+        width:100%; 
+        border-width:0; 
+        background-color:grey;
+    }
+</style>
 
 
 
