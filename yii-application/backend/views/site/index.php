90ece8f3fab7bdda3dd51a98cbafed2bf5639415
HelloNatalia <nataliatucka01@gmail.com>
2023-02-20 19:15:24 +0100

    added images

--- yii-application/backend/views/site/index.php
+++ yii-application/backend/views/site/index.php
@@ -8,7 +8,7 @@ use yii\helpers\Url;
         <div class="col-12 col-md-6 col-lg-4">
             <a class="text-reset text-decoration-none text-center" href="index.php?r=books%2Findex">
                 <div class="card m-5 shadow">
-                    <img class="card-img-top" src="<?=Url::to('@web/assets/site-img/books.jpg')?>" alt="Card image cap">
+                    <img class="card-img-top" src="<?=Url::to('@web/site-img/books.jpg')?>" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">Książki</h5>
                     </div>
@@ -18,7 +18,7 @@ use yii\helpers\Url;
         <div class="col-12 col-md-6 col-lg-4">
             <a class="text-reset text-decoration-none text-center" href="index.php?r=books%2Fauthors">
                 <div class="card m-5 shadow">
-                    <img class="card-img-top" src="<?=Url::to('@web/assets/site-img/authors.jpg')?>" alt="Card image cap">
+                    <img class="card-img-top" src="<?=Url::to('@web/site-img/authors.jpg')?>" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">Autorzy</h5>
                     </div>
@@ -28,7 +28,7 @@ use yii\helpers\Url;
         <div class="col-12 col-md-6 col-lg-4">
             <a class="text-reset text-decoration-none text-center" href="index.php?r=borrow%2Findex">
                 <div class="card m-5 shadow">
-                    <img class="card-img-top" src="<?=Url::to('@web/assets/site-img/borrows.jpg')?>" alt="Card image cap">
+                    <img class="card-img-top" src="<?=Url::to('@web/site-img/borrows.jpg')?>" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">Wypożyczenia</h5>
                     </div>
@@ -38,7 +38,7 @@ use yii\helpers\Url;
         <div class="col-12 col-md-6 col-lg-4">
             <a class="text-reset text-decoration-none text-center" href="index.php?r=readers%2Findex">
                 <div class="card m-5 shadow">
-                    <img class="card-img-top" src="<?=Url::to('@web/assets/site-img/readers.jpg')?>" alt="Card image cap">
+                    <img class="card-img-top" src="<?=Url::to('@web/site-img/readers.jpg')?>" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">Czytelnicy</h5>
                     </div>
@@ -48,7 +48,7 @@ use yii\helpers\Url;
         <div class="col-12 col-md-6 col-lg-4">
             <a class="text-reset text-decoration-none text-center" href="index.php?r=cash%2Findex">
                 <div class="card m-5 shadow">
-                    <img class="card-img-top" src="<?=Url::to('@web/assets/site-img/cash.jpg')?>" alt="Card image cap">
+                    <img class="card-img-top" src="<?=Url::to('@web/site-img/cash.jpg')?>" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">Należności</h5>
                     </div>
@@ -58,7 +58,7 @@ use yii\helpers\Url;
         <div class="col-12 col-md-6 col-lg-4">
             <a class="text-reset text-decoration-none text-center" href="index.php?r=reports%2Findex">
                 <div class="card m-5 shadow">
-                    <img class="card-img-top" src="<?=Url::to('@web/assets/site-img/reports.jpg')?>" alt="Card image cap">
+                    <img class="card-img-top" src="<?=Url::to('@web/site-img/reports.jpg')?>" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">Raporty</h5>
                     </div>
